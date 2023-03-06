453de233d2ffcf76a9f51571f50056ca1dd8245f
Nirajan Acharya <nirajanach@gmail.com>
2023-03-06 17:10:24 +1000

    Complete form component

---
+++ Winforms/Form1.Designer.vb
@@ -0,0 +1,405 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmActDetails
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.lblName = New System.Windows.Forms.Label()
+        Me.lblAddress = New System.Windows.Forms.Label()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.txtAddress = New System.Windows.Forms.TextBox()
+        Me.lblCity = New System.Windows.Forms.Label()
+        Me.txtCity = New System.Windows.Forms.TextBox()
+        Me.lblState = New System.Windows.Forms.Label()
+        Me.txtState = New System.Windows.Forms.TextBox()
+        Me.lblPostCode = New System.Windows.Forms.Label()
+        Me.txtPostCode = New System.Windows.Forms.TextBox()
+        Me.lblContact = New System.Windows.Forms.Label()
+        Me.txtContact = New System.Windows.Forms.TextBox()
+        Me.lblFirstName = New System.Windows.Forms.Label()
+        Me.txtFirstName = New System.Windows.Forms.TextBox()
+        Me.lblLastName = New System.Windows.Forms.Label()
+        Me.txtLastName = New System.Windows.Forms.TextBox()
+        Me.lblMobile1 = New System.Windows.Forms.Label()
+        Me.txtMobile1 = New System.Windows.Forms.TextBox()
+        Me.lblMobile2 = New System.Windows.Forms.Label()
+        Me.txtMobile2 = New System.Windows.Forms.TextBox()
+        Me.lblLandline = New System.Windows.Forms.Label()
+        Me.txtLandline = New System.Windows.Forms.TextBox()
+        Me.lblFax = New System.Windows.Forms.Label()
+        Me.lblEmail = New System.Windows.Forms.Label()
+        Me.lblEmail2 = New System.Windows.Forms.Label()
+        Me.lblWebsite = New System.Windows.Forms.Label()
+        Me.lblABN = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.txtFax = New System.Windows.Forms.TextBox()
+        Me.txtEmail = New System.Windows.Forms.TextBox()
+        Me.txtEmail2 = New System.Windows.Forms.TextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.txtABN = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'lblName
+        '
+        Me.lblName.AutoSize = True
+        Me.lblName.Location = New System.Drawing.Point(65, 41)
+        Me.lblName.Name = "lblName"
+        Me.lblName.Size = New System.Drawing.Size(42, 15)
+        Me.lblName.TabIndex = 0
+        Me.lblName.Text = "Name:"
+        '
+        'lblAddress
+        '
+        Me.lblAddress.AutoSize = True
+        Me.lblAddress.Location = New System.Drawing.Point(55, 83)
+        Me.lblAddress.Name = "lblAddress"
+        Me.lblAddress.Size = New System.Drawing.Size(52, 15)
+        Me.lblAddress.TabIndex = 1
+        Me.lblAddress.Text = "Address:"
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(115, 40)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(188, 23)
+        Me.txtName.TabIndex = 2
+        '
+        'txtAddress
+        '
+        Me.txtAddress.Location = New System.Drawing.Point(115, 80)
+        Me.txtAddress.Multiline = True
+        Me.txtAddress.Name = "txtAddress"
+        Me.txtAddress.Size = New System.Drawing.Size(188, 23)
+        Me.txtAddress.TabIndex = 3
+        '
+        'lblCity
+        '
+        Me.lblCity.AutoSize = True
+        Me.lblCity.Location = New System.Drawing.Point(76, 125)
+        Me.lblCity.Name = "lblCity"
+        Me.lblCity.Size = New System.Drawing.Size(31, 15)
+        Me.lblCity.TabIndex = 4
+        Me.lblCity.Text = "City:"
+        '
+        'txtCity
+        '
+        Me.txtCity.Location = New System.Drawing.Point(115, 120)
+        Me.txtCity.Name = "txtCity"
+        Me.txtCity.Size = New System.Drawing.Size(188, 23)
+        Me.txtCity.TabIndex = 5
+        '
+        'lblState
+        '
+        Me.lblState.AutoSize = True
+        Me.lblState.Location = New System.Drawing.Point(71, 160)
+        Me.lblState.Name = "lblState"
+        Me.lblState.Size = New System.Drawing.Size(36, 15)
+        Me.lblState.TabIndex = 6
+        Me.lblState.Text = "State:"
+        '
+        'txtState
+        '
+        Me.txtState.Location = New System.Drawing.Point(115, 157)
+        Me.txtState.Name = "txtState"
+        Me.txtState.Size = New System.Drawing.Size(52, 23)
+        Me.txtState.TabIndex = 7
+        '
+        'lblPostCode
+        '
+        Me.lblPostCode.AutoSize = True
+        Me.lblPostCode.Location = New System.Drawing.Point(182, 160)
+        Me.lblPostCode.Name = "lblPostCode"
+        Me.lblPostCode.Size = New System.Drawing.Size(64, 15)
+        Me.lblPostCode.TabIndex = 8
+        Me.lblPostCode.Text = "Post Code:"
+        '
+        'txtPostCode
+        '
+        Me.txtPostCode.Location = New System.Drawing.Point(251, 156)
+        Me.txtPostCode.Name = "txtPostCode"
+        Me.txtPostCode.Size = New System.Drawing.Size(52, 23)
+        Me.txtPostCode.TabIndex = 9
+        '
+        'lblContact
+        '
+        Me.lblContact.AutoSize = True
+        Me.lblContact.Location = New System.Drawing.Point(55, 191)
+        Me.lblContact.Name = "lblContact"
+        Me.lblContact.Size = New System.Drawing.Size(52, 15)
+        Me.lblContact.TabIndex = 10
+        Me.lblContact.Text = "Contact:"
+        '
+        'txtContact
+        '
+        Me.txtContact.Location = New System.Drawing.Point(115, 190)
+        Me.txtContact.Name = "txtContact"
+        Me.txtContact.Size = New System.Drawing.Size(188, 23)
+        Me.txtContact.TabIndex = 11
+        '
+        'lblFirstName
+        '
+        Me.lblFirstName.AutoSize = True
+        Me.lblFirstName.Location = New System.Drawing.Point(40, 226)
+        Me.lblFirstName.Name = "lblFirstName"
+        Me.lblFirstName.Size = New System.Drawing.Size(67, 15)
+        Me.lblFirstName.TabIndex = 12
+        Me.lblFirstName.Text = "First Name:"
+        '
+        'txtFirstName
+        '
+        Me.txtFirstName.Location = New System.Drawing.Point(115, 223)
+        Me.txtFirstName.Name = "txtFirstName"
+        Me.txtFirstName.Size = New System.Drawing.Size(188, 23)
+        Me.txtFirstName.TabIndex = 13
+        '
+        'lblLastName
+        '
+        Me.lblLastName.AutoSize = True
+        Me.lblLastName.Location = New System.Drawing.Point(43, 262)
+        Me.lblLastName.Name = "lblLastName"
+        Me.lblLastName.Size = New System.Drawing.Size(66, 15)
+        Me.lblLastName.TabIndex = 14
+        Me.lblLastName.Text = "Last Name:"
+        '
+        'txtLastName
+        '
+        Me.txtLastName.Location = New System.Drawing.Point(115, 254)
+        Me.txtLastName.Name = "txtLastName"
+        Me.txtLastName.Size = New System.Drawing.Size(188, 23)
+        Me.txtLastName.TabIndex = 15
+        '
+        'lblMobile1
+        '
+        Me.lblMobile1.AutoSize = True
+        Me.lblMobile1.Location = New System.Drawing.Point(51, 295)
+        Me.lblMobile1.Name = "lblMobile1"
+        Me.lblMobile1.Size = New System.Drawing.Size(56, 15)
+        Me.lblMobile1.TabIndex = 16
+        Me.lblMobile1.Text = "Mobile 1:"
+        '
+        'txtMobile1
+        '
+        Me.txtMobile1.Location = New System.Drawing.Point(115, 287)
+        Me.txtMobile1.Name = "txtMobile1"
+        Me.txtMobile1.Size = New System.Drawing.Size(188, 23)
+        Me.txtMobile1.TabIndex = 17
+        '
+        'lblMobile2
+        '
+        Me.lblMobile2.AutoSize = True
+        Me.lblMobile2.Location = New System.Drawing.Point(51, 329)
+        Me.lblMobile2.Name = "lblMobile2"
+        Me.lblMobile2.Size = New System.Drawing.Size(56, 15)
+        Me.lblMobile2.TabIndex = 18
+        Me.lblMobile2.Text = "Mobile 2:"
+        '
+        'txtMobile2
+        '
+        Me.txtMobile2.Location = New System.Drawing.Point(115, 321)
+        Me.txtMobile2.Name = "txtMobile2"
+        Me.txtMobile2.Size = New System.Drawing.Size(188, 23)
+        Me.txtMobile2.TabIndex = 19
+        '
+        'lblLandline
+        '
+        Me.lblLandline.AutoSize = True
+        Me.lblLandline.Location = New System.Drawing.Point(52, 362)
+        Me.lblLandline.Name = "lblLandline"
+        Me.lblLandline.Size = New System.Drawing.Size(55, 15)
+        Me.lblLandline.TabIndex = 20
+        Me.lblLandline.Text = "Landline:"
+        '
+        'txtLandline
+        '
+        Me.txtLandline.Location = New System.Drawing.Point(115, 354)
+        Me.txtLandline.Name = "txtLandline"
+        Me.txtLandline.Size = New System.Drawing.Size(188, 23)
+        Me.txtLandline.TabIndex = 21
+        '
+        'lblFax
+        '
+        Me.lblFax.AutoSize = True
+        Me.lblFax.Location = New System.Drawing.Point(79, 393)
+        Me.lblFax.Name = "lblFax"
+        Me.lblFax.Size = New System.Drawing.Size(28, 15)
+        Me.lblFax.TabIndex = 22
+        Me.lblFax.Text = "Fax:"
+        '
+        'lblEmail
+        '
+        Me.lblEmail.AutoSize = True
+        Me.lblEmail.Location = New System.Drawing.Point(68, 429)
+        Me.lblEmail.Name = "lblEmail"
+        Me.lblEmail.Size = New System.Drawing.Size(39, 15)
+        Me.lblEmail.TabIndex = 23
+        Me.lblEmail.Text = "Email:"
+        '
+        'lblEmail2
+        '
+        Me.lblEmail2.AutoSize = True
+        Me.lblEmail2.Location = New System.Drawing.Point(62, 464)
+        Me.lblEmail2.Name = "lblEmail2"
+        Me.lblEmail2.Size = New System.Drawing.Size(45, 15)
+        Me.lblEmail2.TabIndex = 24
+        Me.lblEmail2.Text = "Email2:"
+        '
+        'lblWebsite
+        '
+        Me.lblWebsite.AutoSize = True
+        Me.lblWebsite.Location = New System.Drawing.Point(55, 496)
+        Me.lblWebsite.Name = "lblWebsite"
+        Me.lblWebsite.Size = New System.Drawing.Size(52, 15)
+        Me.lblWebsite.TabIndex = 25
+        Me.lblWebsite.Text = "Website:"
+        '
+        'lblABN
+        '
+        Me.lblABN.AutoSize = True
+        Me.lblABN.Location = New System.Drawing.Point(73, 528)
+        Me.lblABN.Name = "lblABN"
+        Me.lblABN.Size = New System.Drawing.Size(34, 15)
+        Me.lblABN.TabIndex = 26
+        Me.lblABN.Text = "ABN:"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(97, 562)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(181, 54)
+        Me.Button1.TabIndex = 27
+        Me.Button1.Text = "Save All Customer details for All Contacts && Close"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'txtFax
+        '
+        Me.txtFax.Location = New System.Drawing.Point(115, 385)
+        Me.txtFax.Multiline = True
+        Me.txtFax.Name = "txtFax"
+        Me.txtFax.Size = New System.Drawing.Size(188, 23)
+        Me.txtFax.TabIndex = 28
+        '
+        'txtEmail
+        '
+        Me.txtEmail.Location = New System.Drawing.Point(115, 421)
+        Me.txtEmail.Name = "txtEmail"
+        Me.txtEmail.Size = New System.Drawing.Size(188, 23)
+        Me.txtEmail.TabIndex = 29
+        '
+        'txtEmail2
+        '
+        Me.txtEmail2.Location = New System.Drawing.Point(115, 456)
+        Me.txtEmail2.Name = "txtEmail2"
+        Me.txtEmail2.Size = New System.Drawing.Size(188, 23)
+        Me.txtEmail2.TabIndex = 30
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(115, 488)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(188, 23)
+        Me.TextBox1.TabIndex = 31
+        '
+        'txtABN
+        '
+        Me.txtABN.Location = New System.Drawing.Point(115, 520)
+        Me.txtABN.Name = "txtABN"
+        Me.txtABN.Size = New System.Drawing.Size(188, 23)
+        Me.txtABN.TabIndex = 32
+        '
+        'frmActDetails
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 639)
+        Me.Controls.Add(Me.txtABN)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.txtEmail2)
+        Me.Controls.Add(Me.txtEmail)
+        Me.Controls.Add(Me.txtFax)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.lblABN)
+        Me.Controls.Add(Me.lblWebsite)
+        Me.Controls.Add(Me.lblEmail2)
+        Me.Controls.Add(Me.lblEmail)
+        Me.Controls.Add(Me.lblFax)
+        Me.Controls.Add(Me.txtLandline)
+        Me.Controls.Add(Me.lblLandline)
+        Me.Controls.Add(Me.txtMobile2)
+        Me.Controls.Add(Me.lblMobile2)
+        Me.Controls.Add(Me.txtMobile1)
+        Me.Controls.Add(Me.lblMobile1)
+        Me.Controls.Add(Me.txtLastName)
+        Me.Controls.Add(Me.lblLastName)
+        Me.Controls.Add(Me.txtFirstName)
+        Me.Controls.Add(Me.lblFirstName)
+        Me.Controls.Add(Me.txtContact)
+        Me.Controls.Add(Me.lblContact)
+        Me.Controls.Add(Me.txtPostCode)
+        Me.Controls.Add(Me.lblPostCode)
+        Me.Controls.Add(Me.txtState)
+        Me.Controls.Add(Me.lblState)
+        Me.Controls.Add(Me.txtCity)
+        Me.Controls.Add(Me.lblCity)
+        Me.Controls.Add(Me.txtAddress)
+        Me.Controls.Add(Me.txtName)
+        Me.Controls.Add(Me.lblAddress)
+        Me.Controls.Add(Me.lblName)
+        Me.Name = "frmActDetails"
+        Me.Text = "Account Details"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblName As Label
+    Friend WithEvents lblAddress As Label
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents txtAddress As TextBox
+    Friend WithEvents lblCity As Label
+    Friend WithEvents txtCity As TextBox
+    Friend WithEvents lblState As Label
+    Friend WithEvents txtState As TextBox
+    Friend WithEvents lblPostCode As Label
+    Friend WithEvents txtPostCode As TextBox
+    Friend WithEvents lblContact As Label
+    Friend WithEvents txtContact As TextBox
+    Friend WithEvents lblFirstName As Label
+    Friend WithEvents txtFirstName As TextBox
+    Friend WithEvents lblLastName As Label
+    Friend WithEvents txtLastName As TextBox
+    Friend WithEvents lblMobile1 As Label
+    Friend WithEvents txtMobile1 As TextBox
+    Friend WithEvents lblMobile2 As Label
+    Friend WithEvents txtMobile2 As TextBox
+    Friend WithEvents lblLandline As Label
+    Friend WithEvents txtLandline As TextBox
+    Friend WithEvents lblFax As Label
+    Friend WithEvents lblEmail As Label
+    Friend WithEvents lblEmail2 As Label
+    Friend WithEvents lblWebsite As Label
+    Friend WithEvents lblABN As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents txtFax As TextBox
+    Friend WithEvents txtEmail As TextBox
+    Friend WithEvents txtEmail2 As TextBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents txtABN As TextBox
+End Class
---
+++ Winforms/Form1.vb
@@ -0,0 +1,3 @@
+﻿Public Class frmActDetails
+
+End Class
